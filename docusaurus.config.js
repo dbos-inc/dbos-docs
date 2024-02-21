59c1c3458f08e942cdfc4ac48fd10ab2353634dc
Qian Li <qian.li@dbos.dev>
2024-02-21 14:11:33 -0800

    Algolia Search Bar (#53)
    This PR adds a search bar to our docs, which is powered by Algolia.
    The crawler runs every week, but we can also manually refresh it
    whenever we publish our docs.
    
    We are currently using the free plan, but we can upgrade later to use
    more powerful search/recommendation.
    
    Please let me know if you want to access the Algolia dash board.
    
    A screenshot of how it looks like:
    
    ![image](https://github.com/dbos-inc/dbos-docs/assets/11331727/112e7a7f-ad55-4659-a5a3-d9361742b5a9)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,6 +115,35 @@ const config = {
       tableOfContents: {
         maxHeadingLevel:5,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'UY2VKOMIL2',
+
+        // Public API key: it is safe to commit it
+        apiKey: '283b0ed33db1f1f2a1955ba0aa83671f',
+
+        indexName: 'dbos',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'docs\\.dbos\\.dev',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
+      },
     }),
 };
 
