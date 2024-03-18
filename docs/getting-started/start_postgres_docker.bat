48443d0adbba1d8b8f0f9feae2fd9a3dcd45ee19
Joe Coyle <614948+jtcoyle@users.noreply.github.com>
2024-03-18 16:04:20 -0400

    Uploading most recent file
    I have tested this batch file and it works

--- docs/getting-started/start_postgres_docker.bat
+++ docs/getting-started/start_postgres_docker.bat
@@ -1,7 +1,6 @@
 @echo off
 
 setlocal ENABLEEXTENSIONS
-set port=%1
 
 echo Checking if PGPASSWORD is set.
 if "%PGPASSWORD%"=="" (
@@ -9,19 +8,19 @@ if "%PGPASSWORD%"=="" (
   exit /b 1
 )
 
-echo Start Postgres in a local Docker container
+echo Starting PostgreSQL in a local Docker container
 docker run --rm --name=dbos-db --env=POSTGRES_PASSWORD=%PGPASSWORD% --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:16.1
 
 	if %errorlevel% == 125 (
-		echo Error: Check if Docker container already exist
+		echo Error: Check if the Docker container already exists
 		exit /b 1
 		) else (
 		goto :start
 		)
-		
+
 :start
 echo Waiting for PostgreSQL to start...
-for /l %%i in (1,1,40) do (
+for /l %%i in (1,1,30) do (
   docker exec dbos-db psql -U postgres -c "SELECT 1;" >NUL 2>&1
 
 if %errorlevel% equ 0 (
@@ -31,6 +30,7 @@ if %errorlevel% equ 0 (
   )
   timeout /t 1 /nobreak
 )
-  netstat -ano | findstr ":5432" >nul 2>nul
   )
 :break
+
+echo Database started successfully^!
