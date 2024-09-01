c13498ea5a03a36eae239dd2e2db07f9ba1c3e43
Peter Kraft <petereliaskraft@gmail.com>
2024-09-01 14:32:51 -0700

    Better Python Styling (#187)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -152,7 +152,19 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} DBOS, Inc.`,
       },
       prism: {
-        theme: prismThemes.okaidia,
+        theme: {
+          ...prismThemes.okaidia,
+          styles: [
+            ...prismThemes.okaidia.styles,
+            {
+              types: ['punctuation'],
+              languages: ['python'],
+              style: {
+                color: 'rgb(199, 146, 234)', // Change this to your desired color
+              },
+            },
+          ],
+        },
       },
       tableOfContents: {
         maxHeadingLevel:5,
