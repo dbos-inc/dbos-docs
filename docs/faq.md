0a036a99fee0bfbe44927dea86c69da6663814f6
Peter Kraft <petereliaskraft@gmail.com>
2025-06-04 11:56:45 -0700

    start

---
+++ docs/faq.md
@@ -0,0 +1,9 @@
+---
+hide_table_of_contents: false
+hide_title: false
+title: Troubleshooting & FAQ
+---
+
+### Why are my queues stuck?
+
+Because...
--- sidebars.js
+++ sidebars.js
@@ -79,6 +79,11 @@ const sidebars = {
         }
       ],
     },
+    {
+      type: 'doc',
+      id: 'faq',
+      label: 'Troubleshooting & FAQ',
+    },
   ],
 
   examplesSidebar: [
