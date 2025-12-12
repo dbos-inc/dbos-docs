7227b3536e2710e5cb009c36c63f1531dde0c099
Qian Li <qian.li@dbos.dev>
2025-12-12 13:23:35 -0800

    update packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,8 +83,13 @@ const config = {
   projectName: 'dbos-docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+      onBrokenMarkdownImages: 'warn',
+    }
+  },
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
